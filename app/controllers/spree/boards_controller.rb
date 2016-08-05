eab4e26e4c1803e9d5daa8d06b5acc4ef4e17660
Daniel Niedzialkowski <dniedzialkowski@netkodo.com>
2016-08-05 10:49:12 +0200

    back to room after scroll

--- app/controllers/spree/boards_controller.rb
+++ app/controllers/spree/boards_controller.rb
@@ -66,7 +66,15 @@ class Spree::BoardsController < Spree::StoreController
     @room_style = Hash.new(0)
 
     tmp_boards = Spree::Board.published().order("created_at desc")
-    @boards = tmp_boards.page(params[:page]).per(60)
+    if session[:hash_board_id].present? and session[:remember_product_page].present?
+      @jump_to_board_id = session[:hash_board_id]
+      @new_next_page = session[:remember_product_page].to_i + 1
+      @boards = tmp_boards.page(params[:page]).per(4*session[:remember_product_page].to_i)
+      session[:hash_board_id] = nil
+      session[:remember_product_page] = nil
+    else
+      @boards = tmp_boards.page(params[:page]).per(4)
+    end
 
     colors = []
     tmp_boards.each do |b|
@@ -94,6 +102,8 @@ class Spree::BoardsController < Spree::StoreController
   end
 
   def room_page
+    session[:remember_product_page] = params[:page]
+    puts session[:remember_product_page]
     if params[:filter].present?
       tab = []
       params[:filter].except(:color).each do |f|
@@ -101,9 +111,9 @@ class Spree::BoardsController < Spree::StoreController
       end
       params[:filter][:color].present? ? statement="Spree::Board.active.where(#{tab.join(',').present? ? tab.join(',') : 'nil'}).by_color_family(#{params[:filter][:color]})" : statement="Spree::Board.active.where(#{tab.join(',')})"
       tmp_boards = eval(statement)
-      @boards = tmp_boards.page(params[:page]).per(60)
+      @boards = tmp_boards.page(params[:page]).per(4)
     else
-      @boards = Spree::Board.published().order("created_at desc").page(params[:page]).per(60)
+      @boards = Spree::Board.published().order("created_at desc").page(params[:page]).per(4)
     end
     render "room_page", layout: false
   end
@@ -235,6 +245,7 @@ class Spree::BoardsController < Spree::StoreController
   def show
     @board = Spree::Board.friendly.find(params[:id])
     impressionist(@board)
+    session[:hash_board_id] = @board.id
   end
 
   def show_portfolio
