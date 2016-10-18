289dcc6800d158b2dfccb98f73a63b22f98e1e33
Daniel Niedzialkowski <dniedzialkowski@netkodo.com>
2016-10-18 10:08:29 +0200

    new sizne and crop style for portfolio_image

--- app/models/spree/portfolio.rb
+++ app/models/spree/portfolio.rb
@@ -13,7 +13,7 @@ class Spree::Portfolio < ActiveRecord::Base
   validates :name, presence: true
 
   has_attached_file :portfolio_image,
-                    styles: { small: '90x90>',carousel: '200x200>', medium: 'x400>', large: '600x600>' },
+                    styles: { small: '90x90>',carousel: '200x200>', medium: '400x#', large: '600x600>' },
                     default_style: :medium,
                     path: 'portfolio_images/:id/:style/:basename.:extension',
                     convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
