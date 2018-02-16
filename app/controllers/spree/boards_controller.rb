5a5b052c9d6a730bbfa1dd84a3fff63628049cbb
Daniel Niedzialkowski <dniedzialkowski@netkodo.com>
2018-02-16 12:43:51 +0100

    using correct method

--- app/controllers/spree/boards_controller.rb
+++ app/controllers/spree/boards_controller.rb
@@ -142,7 +142,7 @@ class Spree::BoardsController < Spree::StoreController
 
   def profile
     @user = spree_current_user
-    if spree_current_user and (spree_current_user.is_beta_user? or spree_current_user.is_designer?)
+    if spree_current_user and (spree_current_user.is_beta_user? or spree_current_user.is_board_designer?)
 
       @user.user_images.new if @user.user_images.blank?
       @user.marketing_images.new if @user.marketing_images.blank?
